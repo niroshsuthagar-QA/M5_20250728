73907c11f81c14053ab8155cbee3b6626cf8e25c
Nirosh Suthagar <nirosh.suthagar@qa.com>
2025-07-30 14:10:40 +0100

    Flask App Files

---
+++ flask_app_docker/app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def helloWorld():
+    return "Hey! I just used a flask app..."
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
